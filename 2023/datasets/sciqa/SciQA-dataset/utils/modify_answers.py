3942e23ba9986d2de9f2a18678ab429f690b6a79
Debayan Banerjee <debayanin@gmail.com>
2023-07-25 00:18:24 +0200

    Fixed missing questions

--- 2023/datasets/sciqa/SciQA-dataset/utils/modify_answers.py
+++ 2023/datasets/sciqa/SciQA-dataset/utils/modify_answers.py
@@ -1,5 +1,8 @@
 import sys,os,json
 
+
+skip = ['HQ0081','HQ0097','HQ0043''HQ0085','HQ0078','HQ0026','HQ0032','HQ0017','HQ0027','HQ0068']
+
 d = json.loads(open(sys.argv[1]).read())["answers"]
 newarr = []
 for item in d:
@@ -13,6 +16,8 @@ for item in d:
             newarr.append({"id":item["id"], "answer":newanswer})
     elif 'boolean' in answer:
         newarr.append({"id":item["id"], "answer":answer['boolean']})
+    else:
+        newarr.append({"id":item["id"], "answer":[]})
 
 
 f = open(sys.argv[2],'w')
